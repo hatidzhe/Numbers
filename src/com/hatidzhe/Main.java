d3d2c9c0827507bbe37a409d2fdc6b7179f26879
Hatidzhe <yusufova.h@gmail.com>
2015-06-23 09:40:51 +0300

    chg: initial commit of Numbers4

---
+++ src/com/hatidzhe/Main.java
@@ -0,0 +1,25 @@
+package com.hatidzhe;
+import java.util.Scanner;
+import java.lang.Integer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter your number");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        double x;
+        x = Math.log(number) / Math.log(2);
+
+        if (x == Math.floor(x)){
+            System.out.println("The number is a power of two");
+        }
+        else{
+            System.out.println("The number is not a power of two");
+
+
+        }
+    }
+}
